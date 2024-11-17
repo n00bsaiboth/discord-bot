9a96ec0c920ed02ec2d02298fe8b579257a69f5e
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2024-11-17 16:44:11 +0200

    Develop: Updated the node packages

--- README.md
+++ README.md
@@ -15,4 +15,3 @@ Of course you need configs or an API, so you need to create `config.json` file t
 ```
 
 You can get this information from Discord.
-
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
